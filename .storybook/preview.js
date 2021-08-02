2a096261cbd50c5963469a16979a70717b541cfe
Willem <willem.sporen@scenius.nl>
2021-08-02 21:34:04 +0200

    Make it work again

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,6 @@
 import { setCompodocJson } from "@storybook/addon-docs/angular";
 import docJson from "../documentation.json";
 
-// Global CSS
-import '../node_modules/tailwindcss/base.css';
-import '../node_modules/tailwindcss/tailwind.css';
-
 setCompodocJson(docJson);
 
 export const parameters = {
