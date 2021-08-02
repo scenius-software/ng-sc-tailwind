09da5171442ed34a907b0c0d8a96608284d9c9c9
Indy Spaan <indy@scenius.nl>
2021-08-02 21:56:11 +0200

    Proper blur but now the button toggle is broken, heck

--- projects/sc-tailwind/src/lib/directives/menu-dropdown/sc-tw-dropdown-content.directive.ts
+++ projects/sc-tailwind/src/lib/directives/menu-dropdown/sc-tw-dropdown-content.directive.ts
@@ -1,9 +1,30 @@
-import { Directive, Input, OnInit } from '@angular/core';
-import { Subject } from "rxjs";
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  Input,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Directive({
-  selector: '[sc-tw-dropdown-content]'
+  selector: '[sc-tw-dropdown-content]',
 })
 export class ScTwDropdownContentDirective {
-  constructor() { }
+  $blurred = new Subject<boolean>();
+
+  constructor(private renderer: Renderer2, private element: ElementRef) {}
+
+  @HostListener('document:mousedown', ['$event'])
+  onBlur(event: any) {
+    console.log(
+      'contains?>: ',
+      !this.element.nativeElement.contains(event.target)
+    );
+    if (!this.element.nativeElement.contains(event.target)) {
+      // clicked outside => close dropdown list
+      this.$blurred.next();
+    }
+  }
 }
