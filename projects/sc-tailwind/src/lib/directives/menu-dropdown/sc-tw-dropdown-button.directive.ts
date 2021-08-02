e8ece18f1a43c7081ff16ba41104b01efa2abc1f
Indy Spaan <indy@scenius.nl>
2021-08-02 20:19:24 +0200

    Add blur functionality top dropdown

--- projects/sc-tailwind/src/lib/directives/menu-dropdown/sc-tw-dropdown-button.directive.ts
+++ projects/sc-tailwind/src/lib/directives/menu-dropdown/sc-tw-dropdown-button.directive.ts
@@ -1,17 +1,31 @@
-import { Directive, ElementRef, HostListener, OnDestroy, Renderer2 } from '@angular/core';
-import { Subject } from "rxjs";
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  OnDestroy,
+  Renderer2,
+} from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Directive({
-  selector: '[sc-tw-dropdown-button]'
+  selector: '[sc-tw-dropdown-button]',
 })
 export class ScTwDropdownButtonDirective implements OnDestroy {
   $clicked = new Subject<boolean>();
+  $blurred = new Subject<boolean>();
 
-  constructor(private renderer: Renderer2, element: ElementRef) {
-  }
+  constructor(private renderer: Renderer2, private element: ElementRef) {}
 
   @HostListener('click') onClick() {
     this.$clicked.next();
+  }
+
+  @HostListener('document:mousedown', ['$event'])
+  onBlur(event: any) {
+    if (!this.element.nativeElement.contains(event.target)) {
+      // clicked outside => close dropdown list
+      this.$blurred.next();
+    }
   }
 
   ngOnDestroy(): void {
--- projects/sc-tailwind/src/lib/directives/menu-dropdown/sc-tw-dropdown.component.ts
+++ projects/sc-tailwind/src/lib/directives/menu-dropdown/sc-tw-dropdown.component.ts
@@ -4,32 +4,44 @@ import {
   ChangeDetectorRef,
   Component,
   ContentChild,
-  ElementRef, Renderer2
+  ElementRef,
+  HostListener,
+  Renderer2,
 } from '@angular/core';
-import { ScTwDropdownButtonDirective } from "./sc-tw-dropdown-button.directive";
-import { ScTwDropdownContentDirective } from "./sc-tw-dropdown-content.directive";
-import { DropdownAnimationTransition } from "./sc-tw-dropdown.animations";
+import { ScTwDropdownButtonDirective } from './sc-tw-dropdown-button.directive';
+import { ScTwDropdownContentDirective } from './sc-tw-dropdown-content.directive';
+import { DropdownAnimationTransition } from './sc-tw-dropdown.animations';
 
 @Component({
   selector: 'sc-tw-dropdown',
   templateUrl: './sc-tw-dropdown.component.html',
-  styleUrls: [ './sc-tw-dropdown.component.scss' ],
-  animations: [
-    DropdownAnimationTransition
-  ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./sc-tw-dropdown.component.scss'],
+  animations: [DropdownAnimationTransition],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ScTwDropdownComponent implements AfterContentInit {
-  @ContentChild(ScTwDropdownButtonDirective) buttonChild!: ScTwDropdownButtonDirective;
-  @ContentChild(ScTwDropdownContentDirective, { read: ElementRef }) contentChild!: ScTwDropdownContentDirective;
+  @ContentChild(ScTwDropdownButtonDirective)
+  buttonChild!: ScTwDropdownButtonDirective;
+  @ContentChild(ScTwDropdownContentDirective, { read: ElementRef })
+  contentChild!: ScTwDropdownContentDirective;
 
   showContent = false;
 
-  constructor(private _cdRef: ChangeDetectorRef, private _renderer: Renderer2) {
-  }
+  constructor(
+    private _cdRef: ChangeDetectorRef,
+    private _renderer: Renderer2
+  ) {}
 
   ngAfterContentInit(): void {
-    this.buttonChild?.$clicked?.subscribe(x => this.buttonClicked());
+    this.buttonChild?.$clicked?.subscribe((x) => this.buttonClicked());
+    this.buttonChild?.$blurred?.subscribe((x) => this.buttonBlurred());
+  }
+
+  private buttonBlurred() {
+    if (this.contentChild) {
+      this.showContent = false;
+      this._cdRef.markForCheck();
+    }
   }
 
   private buttonClicked() {
