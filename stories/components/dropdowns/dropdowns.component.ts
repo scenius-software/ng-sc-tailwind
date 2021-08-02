eaba1e0cd5bb61d6436d3ddf80457a849eb8ae87
Indy Spaan <indy@scenius.nl>
2021-08-02 21:44:54 +0200

    Make more exciting

--- stories/components/dropdowns/dropdowns.component.ts
+++ stories/components/dropdowns/dropdowns.component.ts
@@ -8,7 +8,18 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
         <button
           sc-tw-dropdown-button
           type="button"
-          class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
+          class="inline-flex justiium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
+          fy-center
+          w-full
+          rounded-md
+          border
+          border-gray-300
+          shadow-sm
+          px-4
+          py-2
+          bg-white
+          text-sm
+          font-med
           id="menu-button"
           aria-expanded="true"
           aria-haspopup="true"
@@ -30,7 +41,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
         </button>
         <div
           sc-tw-dropdown-content
-          class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+          class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none divide-y divide-gray-100"
           role="menu"
           aria-orientation="vertical"
           aria-labelledby="menu-button"
