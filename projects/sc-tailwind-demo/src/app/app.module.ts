c8bcd934ad0517c1b7529e465aae1eef39556d22
Willem <willem.sporen@scenius.nl>
2021-08-05 00:04:02 +0200

    Attempt 2

--- projects/sc-tailwind-demo/src/app/app.module.ts
+++ projects/sc-tailwind-demo/src/app/app.module.ts
@@ -6,14 +6,12 @@ import { AppComponent } from './app.component';
 import { ScTailwindModule } from "../../../sc-tailwind/src/lib/sc-tailwind.module";
 import { MenuDropdownComponent } from './pages/Menu/menu-dropdown/menu-dropdown.component';
 import { FormToggleComponent } from './pages/Form/form-toggle/form-toggle.component';
-import { ScTwModalBackdropComponent } from "../../../sc-tailwind/src/lib/modals/sc-tw-modal-backdrop/sc-tw-modal-backdrop.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuDropdownComponent,
     FormToggleComponent,
-    ScTwModalBackdropComponent
   ],
   imports: [
     BrowserModule,
